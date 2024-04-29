ac6b08371353c6285bcc936f967d749716f95457
yangrangrang <gkssk2309@gmail.com>
2024-04-29 23:05:02 +0900

    [inflearnJpa1] init

---
+++ inflearnJpa1/src/main/java/jpabook/jpashop/InflearnJpa1Application.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnJpa1Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(InflearnJpa1Application.class, args);
+    }
+
+}
