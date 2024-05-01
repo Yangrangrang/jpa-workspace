11eb80b23ef448d28f370756516d357f18c730b2
yangrangrang <gkssk2309@gmail.com>
2024-05-01 21:37:30 +0900

    #5 - entity 설계
    Member, Order, OrderItem, Address, Delivery, Item 클래스 생성
    enum. 타입의 OrderStatus, DeliveryStatus 생성
    Item 의 경우 싱글테이블로 Book, Album, Movie에 상속
    
    #5 - entity 설계
    
    Member, Order, OrderItem, Address, Delivery, Item 클래스 생성
    enum. 타입의 OrderStatus, DeliveryStatus 생성
    Item 의 경우 싱글테이블로 Book, Album, Movie에 상속

---
+++ inflearnJpa1/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // 내가 매핑을 하는게 아니고 나는 매핑된 거울을 뿐이야
+    private List<Order> orders = new ArrayList<>();
+
+}
