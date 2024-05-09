e053562ea112762d80298c234c7d80c22e7285ab
yangrangrang <gkssk2309@gmail.com>
2024-05-09 16:24:40 +0900

    #7 - Member Service 코드 추가
    (Member Repository 수정 (sping jpa를 쓰게 되면 `EntityManager`를 `@RequiredArgsConstructor` 로 생성자 주입으로 사용 가능)

---
+++ inflearnJpa1/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,57 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)     // 데이터 변경하는 것은 트랜잭션이 꼭 있어야함. 어노테이션이 2개가 있는데 spring 어노테이션을 쓰는걸 권장
+                                    // 읽기에는 가급적이면 readOnly를 넣어주면 됨.(기본으로 이렇게 전체에 넣고)
+public class MemberService {
+
+    private final MemberRepository memberRepository;    // final 권장, 컴파일 시점에 체크를 할 수 있기 때문에
+
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    /**
+     * 회원가입
+     */
+    @Transactional  // (읽기 제외는 readOnly를 true하면 안되기 때문에 따로 넣어줌)
+    public Long join(Member member) {
+        validateDuplicateMember(member);    // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        // EXCEPTION
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     */
+//    @Transactional(readOnly = true) // 읽기에는 가급적이면 readOnly를 넣어주면 됨.
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 단건 조회
+     */
+//    @Transactional(readOnly = true)
+    public Member findOne(Long id) {
+        return memberRepository.findOne(id);
+    }
+}
