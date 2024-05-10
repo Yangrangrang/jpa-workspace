f3cebc1171100c7952d521eb96824d15111e8ac6
yangrangrang <gkssk2309@gmail.com>
2024-05-10 21:52:09 +0900

    #7 - Member Service Test 코드 작성
    JUnit5로 하기 때문에 `@Test(expected= )` 사용할 수 없음

--- inflearnJpa1/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ inflearnJpa1/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -10,29 +10,29 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
 
-@ExtendWith(SpringExtension.class)  // JUnit5
-@SpringBootTest
-class MemberRepositoryTest {
-
-    @Autowired
-    MemberRepository memberRepository;
-
-    @Test
-    @Transactional // test에 있으면 롤백함
-    @Rollback(value = false)    // 롤백이 안되서 디비에서 확인이 됨
-    public void testMember() throws Exception{
-        //given
-        Member member = new Member();
-        member.setUsername("memberA");
-
-        //when
-        Long saveId = memberRepository.save(member);
-        Member findMember = memberRepository.find(saveId);
-
-        //then
-        assertThat(findMember.getId()).isEqualTo(member.getId());
-        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        System.out.println(findMember == member);   // 영속성컨텍스트에서 식별자가 같으면 같은 엔티티로 인식
-        assertThat(findMember).isEqualTo(member);   // true
-    }
-}
+//@ExtendWith(SpringExtension.class)  // JUnit5
+//@SpringBootTest
+//class MemberRepositoryTest {
+//
+//    @Autowired
+//    MemberRepository memberRepository;
+//
+//    @Test
+//    @Transactional // test에 있으면 롤백함
+//    @Rollback(value = false)    // 롤백이 안되서 디비에서 확인이 됨
+//    public void testMember() throws Exception{
+//        //given
+//        Member member = new Member();
+//        member.setUsername("memberA");
+//
+//        //when
+//        Long saveId = memberRepository.save(member);
+//        Member findMember = memberRepository.find(saveId);
+//
+//        //then
+//        assertThat(findMember.getId()).isEqualTo(member.getId());
+//        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+//        System.out.println(findMember == member);   // 영속성컨텍스트에서 식별자가 같으면 같은 엔티티로 인식
+//        assertThat(findMember).isEqualTo(member);   // true
+//    }
+//}
