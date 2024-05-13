9485ba65430f4634f12777344e2972c86576e6e5
yangrangrang <gkssk2309@gmail.com>
2024-05-13 09:16:51 +0900

    #9 - 상품 엔티티 수정
    엔티티에 stock 추가,제거 비즈니스 로직 추가

--- inflearnJpa1/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ inflearnJpa1/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NullEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비즈니스 로직 //   엔티티 자체에 해결하는 부분은 엔티티 자체에 비즈니스 로직을 넣는게 객체 지향적
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NullEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
