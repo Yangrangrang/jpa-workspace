d54ed12c8ab7b73939d340daa66d6622116ae3c2
yangrangrang <gkssk2309@gmail.com>
2024-05-14 14:10:09 +0900

    #11 - 주문, 주문 상품 엔티티 로직 추가

--- inflearnJpa1/src/main/java/jpabook/jpashop/domain/Order.java
+++ inflearnJpa1/src/main/java/jpabook/jpashop/domain/Order.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -34,7 +35,7 @@ public class Order {
     private LocalDateTime orderDate;    // 주문 시간
 
     @Enumerated(EnumType.STRING)
-    private OrderStatus orderStatue;    // 주문상태 [ORDER, CANCEL]
+    private OrderStatus statue;    // 주문상태 [ORDER, CANCEL]
 
     // == 연관관계 메서드 ==
     public void setMember(Member member) {
@@ -42,8 +43,49 @@ public class Order {
         member.getOrders().add(this);
     }
 
-    public void addOrderOtem(OrderItem orderItem) {
+    public void addOrderItem(OrderItem orderItem) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    // == 생성 메서드 == //
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatue(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    // == 비즈니스 로직 == //
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatue(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    // == 조회 로직 == //
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
     }
 }
