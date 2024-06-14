c790947404ba3bd674840907178288efceea81c9
yangrangrang <gkssk2309@gmail.com>
2024-06-14 09:11:25 +0900

    #15 - API로 회원등록.
    v1 : Entity를 바로 v2: DTO생성해서
    Entity와 매핑되어있을 경우, 변수명을 변경하였을때 API스펙자체가 바뀌는 에러가 발생함.
    무조건 DTO만들어서 작업해야함.

---
+++ inflearnJpa1/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,47 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members") // 유일한 장점은 class를 안만들어도 된다. Entity를 변경하게 되면 API 스펙이 변경됨.
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members") // DTO 만듬.
+    public CreateMemberResponse saveMember2(@RequestBody @Valid CreateMemberRequest request) {
+
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+}
