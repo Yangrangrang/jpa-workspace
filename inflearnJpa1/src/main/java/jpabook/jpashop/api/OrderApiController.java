48a4acaf6c4b72f223e0ffe97f4312646d35d0c7
yangrangrang <gkssk2309@gmail.com>
2024-07-23 19:19:22 +0900

    #19 - v2. 엔티티를 DTO로 변환
    이 부분에서 OrderDto class 에 보면
    orderItem은 엔티티로 넘어가기 때문에 완전히 DTO로 감싼게 아니다.
    즉, 엔티티가 노출 됨.

--- inflearnJpa1/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ inflearnJpa1/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,16 +1,21 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -42,4 +47,42 @@ public class OrderApiController {
         }
         return all;
     }
+
+    /**
+     * v2. DTO 로 변환
+     */
+    @GetMapping("v2/orders")
+    public List<OrderDto> ordersV2() {
+        log.info(">>>>>>>> v2/orders");
+
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return collect;
+    }
+
+    @Getter
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItem> orderItems;
+
+        public OrderDto (Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            order.getOrderItems().stream().forEach(o -> o.getItem().getName());
+            orderItems = order.getOrderItems();     // 이렇게만 하면 엔티티 이기때문에 결과가 null로 나옴. 위에 stream으로 반복문을 돌려줘야 원하는 값을 얻음.
+            
+        }
+    }
+
 }
