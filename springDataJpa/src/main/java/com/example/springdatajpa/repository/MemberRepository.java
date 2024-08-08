3eea5a75434e55bdf73d105d3f54435add59d6b8
yangrangrang <gkssk2309@gmail.com>
2024-08-08 13:24:08 +0900

    #25 - JPA NamedQuery
    네임드 쿼리가 가지는 큰 장점 : 애플리케이션 로딩 시점에 한번 파싱을 함, 그래서 오류가 있을 시 문법오류를 알려줌.(정적쿼리)
    하지만 실무에서 namedQuery 잘 안씀.

--- springDataJpa/src/main/java/com/example/springdatajpa/repository/MemberRepository.java
+++ springDataJpa/src/main/java/com/example/springdatajpa/repository/MemberRepository.java
@@ -2,10 +2,15 @@ package com.example.springdatajpa.repository;
 
 import com.example.springdatajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+//    @Query(name = "Member.findByUsername")        // 없애도 잘 동작함, 네임트 쿼리가 우선순위
+    List<Member> findByUsername(@Param("username") String username);
 }
