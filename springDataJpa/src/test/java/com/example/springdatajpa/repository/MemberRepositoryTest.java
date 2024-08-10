c4517dd159ed6e81b26e4e5ec9a86d28fd29c1e8
yangrangrang <gkssk2309@gmail.com>
2024-08-10 21:31:47 +0900

    #25 - @Query, 값,DTO 조회하기
    @Query 어노테이션을 사용해서 값과 DTO로 조회하고 테스트 코드 작성

--- springDataJpa/src/test/java/com/example/springdatajpa/repository/MemberRepositoryTest.java
+++ springDataJpa/src/test/java/com/example/springdatajpa/repository/MemberRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.example.springdatajpa.repository;
 
+import com.example.springdatajpa.dto.MemberDto;
 import com.example.springdatajpa.entity.Member;
+import com.example.springdatajpa.entity.Team;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +19,9 @@ class MemberRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
     public void testMember() {
@@ -96,5 +101,32 @@ class MemberRepositoryTest {
         List<Member> result = memberRepository.findUser("AAA",10);
 
         assertThat(result.get(0)).isEqualTo(m1);
+    }
+
+    @Test
+    public void findUserList() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<String> usernameList = memberRepository.findUsernameList();
+        for (String s : usernameList) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void findMemberDto() {
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+        Member m1 = new Member("AAA", 10);
+        m1.changeTeam(team);
+        memberRepository.save(m1);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+        for (MemberDto dto : memberDto) {
+            System.out.println("dto = " + dto);
+        }
     }
 }
