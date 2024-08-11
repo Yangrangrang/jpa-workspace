d425c551988fcc7ab29df8c84ae9dc2984f52de3
yangrangrang <gkssk2309@gmail.com>
2024-08-11 21:06:02 +0900

    #25 - 파라미터 바인딩

--- springDataJpa/src/test/java/com/example/springdatajpa/repository/MemberRepositoryTest.java
+++ springDataJpa/src/test/java/com/example/springdatajpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -127,6 +128,19 @@ class MemberRepositoryTest {
         List<MemberDto> memberDto = memberRepository.findMemberDto();
         for (MemberDto dto : memberDto) {
             System.out.println("dto = " + dto);
+        }
+    }
+
+    @Test
+    public void findByNames() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA","BBB"));
+        for (Member member : result) {
+            System.out.println("member = " + member);
         }
     }
 }
