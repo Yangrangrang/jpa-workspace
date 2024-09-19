1181a75e09c3f123ac097f808aa6b49923e72d0e
yangrangrang <gkssk2309@gmail.com>
2024-09-19 20:12:43 +0900

    #37 - 패치조인

--- querydsl/src/test/java/com/example/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/com/example/querydsl/QuerydslBasicTest.java
@@ -1,13 +1,13 @@
 package com.example.querydsl;
 
 import com.example.querydsl.entity.Member;
-import com.example.querydsl.entity.QMember;
-import com.example.querydsl.entity.QTeam;
 import com.example.querydsl.entity.Team;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceUnit;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -285,5 +285,37 @@ public class QuerydslBasicTest {
         for (Tuple tuple : result) {
             System.out.println("tuple = " + tuple);
         }
+    }
+
+    @PersistenceUnit    // EntityManager 를 만드는 팩토리
+    EntityManagerFactory emf;
+
+    @Test
+    public void fetchJoinNo() {
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());// 이미 로딩 된 엔티티인지, 아직 로딩이 안된 초기화가 안된 인티티 인지 알려줌
+        assertThat(loaded).as("패치 조인 미적용").isFalse();
+    }
+
+    @Test
+    public void fetchJoinUse() {
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .join(member.team, team).fetchJoin()
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());// 이미 로딩 된 엔티티인지, 아직 로딩이 안된 초기화가 안된 인티티 인지 알려줌
+        assertThat(loaded).as("패치 조인 미적용").isTrue();
     }
 }
