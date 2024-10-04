a7e2db9e3a36f8a6b4a0a4159f5062b1f273f511
yangrangrang <gkssk2309@gmail.com>
2024-10-04 20:42:41 +0900

    #41 - 순수 JPA 리포지토리와 Querydsl
    - 순수 JPA 로 memberRepository 생성

---
+++ querydsl/src/test/java/com/example/querydsl/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.example.querydsl.repository;
+
+import com.example.querydsl.entity.Member;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void basicTest() {
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.findById(member.getId()).get();
+        assertThat(findMember).isEqualTo(member);   // 같은 영속성 컨텍스트이기 때문에 객체 주소가 같다고 나와야 함.
+
+        List<Member> result1 = memberJpaRepository.findAll();
+        assertThat(result1).containsExactly(member);
+
+        List<Member> result2 = memberJpaRepository.findByUsername("member1");
+        assertThat(result2).containsExactly(member);
+    }
+}
